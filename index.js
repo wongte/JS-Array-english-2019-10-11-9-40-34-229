a3fc645ee1397e8da39a7250ef962acb43703be9
wongte <56995724+wongte@users.noreply.github.com>
2019-11-10 11:50:31 +0800

    complete index.js

--- index.js
+++ index.js
@@ -1,26 +1,37 @@
 // The writer determines whether the following variables are of type array.
 var a = '[a, b, c, d]';
 var b = [1, 2, 3, 4];
-//TODO
-
+console.log(Array.isArray(a));
+console.log(Array.isArray(b));
 
 // Write a program that multiplies each entry in the following array by 2。
 var a = [1, 2, 3, 4, 5];
-// TODO should output [2,4,6,8,10]
-
+a.map(item => item * 2);
+console.log(a);
 
 // Write the program, according to the following requirements output results.
 var colors = ["Red", "Green", "White", "Black"];
 //TODO case 1 output: 'Red Green White Black'
+console.log(colors.join(' '));
 // case 2 output: 'Red+Green+White+Black'
+console.log(colors.join('+'));
 // case 3 output: 'Red,Green,White,Black'
+console.log(colors.join(','));
 
 
 // Write a program to sort the Numbers in the following array from largest to smallest.
 var a = [5, 1, 8, 10, 4];
-//TODO should output: [10,8,5,4,1]
+a.sort((a,b) => a< b);
+console.log(a);
 
 
 // Program to find the most frequent element in the following array.
 var a = [3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3];
-//TODO should output: 'a'
+const reducer = (counter, value) => {
+  counter[value] = typeof(counter[value]) === 'undefined' ? 1 : counter[value] + 1
+  return counter
+}
+let counter = a.reduce(reducer, {}))
+let counter_arr = Object.keys(counter)
+counter_arr.sort((i,j) => counter[i] < counter[j])
+console.log(counter_arr[0])
